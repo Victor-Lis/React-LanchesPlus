9f36e27c68a5160d7e1001a83391743fe8212c18
Victor-Lis <victorlisbronzo1@gmail.com>
2023-11-04 17:13:13 -0300

    v1

--- src/Connections/firebaseConfig.js
+++ src/Connections/firebaseConfig.js
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, set, get } from "firebase/database";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBoRVeP8zoNJo-BTQB2kuGt7mfWWNgyFzk",
-    authDomain: "lanches-website.firebaseapp.com",
-    databaseURL: "https://lanches-website-default-rtdb.firebaseio.com",
-    projectId: "lanches-website",
-    storageBucket: "lanches-website.appspot.com",
-    messagingSenderId: "985383985789",
-    appId: "1:985383985789:web:6713e954352c6fa44b4714",
-    measurementId: "G-R58NM39WQE"
-};
+import { firebaseConfig } from "./config";
 
 const app = initializeApp(firebaseConfig);
 
